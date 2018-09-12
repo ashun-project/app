7733b11cfb4660ebebfddcb9c50af17eeddab8c4
ashun1 <1948049247@qq.com>
2018-09-12 18:25:58 +0800

    x

--- index.js
+++ index.js
@@ -11,16 +11,23 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'ashun666',
-    database: 'down_list'
+    database: 'user'
 });
 
 app.use(cookie());
 app.use(session({
+    // resave: false, // 是指每次请求都重新设置session cookie，假设你的cookie是6000毫秒过期，每次请求都会再设置6000毫秒
+    // saveUninitialized: false, // 是指无论有没有session cookie，每次请求都设置个session cookie
+    // // store: new FileStore(), // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+    // secret: '123456', //  加密
+    // name: identityKey, //这里的name值得是cookie的name，默认cookie的name是：connect.sid
+    // cookie: {
+    //     maxAge: 16000
+    // }, //设置maxAge是80000ms，即80s后session和相应的cookie失效过期
     resave: true, // 是指每次请求都重新设置session cookie，假设你的cookie是6000毫秒过期，每次请求都会再设置6000毫秒
-    saveUninitialized: false, // 是指无论有没有session cookie，每次请求都设置个session cookie
-    store: new FileStore(), // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+    saveUninitialized: false, // 是指无论有没有session cookie，每次请求都设置个session cookie 
     secret: '123456', //  加密
-    name: identityKey, //这里的name值得是cookie的name，默认cookie的name是：connect.sid
+    name: 'testapp', //这里的name值得是cookie的name，默认cookie的name是：connect.sid
     cookie: {
         maxAge: 16000
     }, //设置maxAge是80000ms，即80s后session和相应的cookie失效过期
@@ -30,39 +37,127 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(express.static(path.resolve(__dirname, './static')));
-
-var myUser = {
-    userName: 'ashun',
-    password: '123456'
+function vaidParams(userName, password) {
+    var error = '';
+    if (!userName || !password) {
+        error = '用户或密码不能为空';
+    }
+    if (userName.length > 12 || password.length > 12) {
+        error = '用户或密码不可超过12位';
+    }
+    if (userName.length < 3 || password.length < 3) {
+        error = '用户或密码不可小于3位';
+    }
+    return error;
 }
 
-app.get('/aaa', function (req, res, next) {
-    var sess = req.session;
-    var loginUser = sess.loginUser;
-    var isLogined = !!loginUser;
-    console.log(sess, 'sess==========')
-    res.json({
-        isLogined: isLogined,
-        name: loginUser || ''
+// 接口
+app.post('/vdidLogin', function (req, res, next) {
+    if (req.session.loginUser) {
+        res.json(req.session.loginUser);
+    } else {
+        res.json({error: 'no'});
+    }
+});
+
+app.post('/register', function (req, res) {
+    var userName = req.body.userName? req.body.userName.replace(/(^\s*)|(\s*$)/g, "") : '';
+    var err = vaidParams(userName, req.body.password);
+    var sql = 'SELECT * FROM list where userName = "'+ userName + '"';
+    var sql2 = "INSERT INTO list(userName, password) VALUES (?, ?)";
+    if (err) {
+        res.json({error: err});
+        return;
+    }
+    pool.getConnection(function (err, conn) {
+        if (err) console.log("POOL register==> " + err);
+        conn.query(sql, function (err, result) {
+            if (err) {
+                console.log('register - ', err.message);
+                res.json({error: '系统出错请重新操作'});
+                conn.release();
+            } else {
+                if (!result[0]) {
+                    conn.query(sql2, [userName, req.body.password], function (err1, result1) {
+                        if (err1) {
+                            console.log('register1- ', err1.message);
+                            res.json({error: '系统出错请重新操作'});
+                        }  else {
+                            // req.session.regenerate(function (err) {
+                            //     if (err) {
+                            //         return res.json({error: '登录失败' });
+                            //     }
+                            //     req.session.loginUser = {userName: userName};
+                            //     res.json({userName: userName});
+                            // });
+                            req.session.loginUser = {userName: userName};
+                            res.json({userName: userName});
+                        }
+                        conn.release();
+                    });
+                } else {
+                    res.json({error: '用户已存在'});
+                    conn.release();
+                }
+            }
+        });
     });
 });
 
 app.post('/login', function (req, res, next) {
-    var user = '';
-    if (req.body.name === myUser.userName && req.body.password === myUser.password) {
-        user = true;
+    // 获取所有列表
+    var userName = req.body.userName? req.body.userName.replace(/(^\s*)|(\s*$)/g, "") : '';
+    var err = vaidParams(userName, req.body.password);
+    var sql = 'SELECT * FROM list where userName = "'+ userName + '"';
+    if (err) {
+        res.json({error: err});
+        return;
     }
-    if (user) {
-        req.session.regenerate(function (err) {
+    pool.getConnection(function (err, conn) {
+        if (err) console.log("POOL login==> " + err);
+        conn.query(sql, function (err, result) {
             if (err) {
-                return res.json({ ret_code: 2, ret_msg: '登录失败' });
+                console.log('login--', err.message);
+                res.json({error: '系统出错请重新操作'});
+            } else {
+                if (result.length) {
+                    if (req.body.password === result[0].password) {
+                        // req.session.regenerate(function (err) {
+                        //     if (err) {
+                        //         res.json({error: '登录失败' });
+                        //     } else {
+                        //         delete result[0].password;
+                        //         req.session.loginUser = result[0];
+                        //         res.json(result[0]);
+                        //     }
+                        // });
+                        delete result[0].password;
+                        req.session.loginUser = result[0];
+                        res.json(result[0]);
+                    } else {
+                        res.json({error: '用户或密码不正确'});
+                    }
+                } else {
+                    res.json({error: '用户不存在'});
+                }
             }
-            req.session.loginUser = '123456';
-            res.json({ ret_code: 0, ret_msg: '登录成功' });
+            conn.release();
         });
-    } else {
-        res.json({ ret_code: 1, ret_msg: '账号或密码错误' });
-    }
+    });
+    // if (req.body.name === myUser.userName && req.body.password === myUser.password) {
+    //     user = true;
+    // }
+    // if (user) {
+    //     req.session.regenerate(function (err) {
+    //         if (err) {
+    //             return res.json({ ret_code: 2, ret_msg: '登录失败' });
+    //         }
+    //         req.session.loginUser = '123456';
+    //         res.json({ ret_code: 0, ret_msg: '登录成功' });
+    //     });
+    // } else {
+    //     res.json({ ret_code: 1, ret_msg: '账号或密码错误' });
+    // }
 });
 
 // 退出登录
@@ -74,12 +169,12 @@ app.post('/logout', function (req, res, next) {
 
     req.session.destroy(function (err) {
         if (err) {
-            res.json({ ret_code: 2, ret_msg: '退出登录失败' });
+            res.json({ error: '退出登录失败' });
             return;
         }
         req.session.loginUser = null;
         res.clearCookie(identityKey);
-        res.json({aa:'退出成功'});
+        res.json({success:'退出成功'});
     });
 });
 
--- static/js/common.js
+++ static/js/common.js
@@ -2,8 +2,14 @@ var modal = document.getElementsByClassName('modal');
 var mask = document.getElementsByClassName('mask')[0];
 var loginMOdel = document.getElementsByClassName('login')[0];
 var registerMOdel = document.getElementsByClassName('register')[0];
-
+var loginBtn = document.querySelector('.login button');
+var registerBtn = document.querySelector('.register button');
+var loginForm = document.getElementById('login-form');
+var registerForm = document.getElementById('register-form');
+getVaidlogin();
 mask.addEventListener('click', closeModal);
+loginBtn.addEventListener('click', getLogin);
+registerBtn.addEventListener('click', getRegister);
 function showMOdel(type) {
     if(type === 'login') {
         loginMOdel.style.display = 'block';
@@ -27,4 +33,86 @@ function search (id) {
     } else {
         window.location.href = '/';
     }
+}
+function getLogin() {
+    var userName = loginForm.userName.value;
+    var password = loginForm.password.value;
+    var obj = {userName: userName, password: password };
+    vaidParams(obj, '/login');
+}
+function getRegister() {
+    var userName = registerForm.userName.value;
+    var password = registerForm.password.value;
+    var vaidPassword = registerForm.vaidPassword.value;
+    if (vaidPassword !== password) {
+        alert('两次密码输入不一致');
+        return;
+    }
+    var obj = {userName: userName, password: password };
+    vaidParams(obj, '/register');
+}
+function vaidParams(obj, url) {
+    var error = '';
+    for (var key in obj) {
+        if (!obj[key]) {
+            error = '用户或密码不能为空';
+        }
+        if (obj[key].length > 12) {
+            error = '用户或密码不可超过12位';
+        }
+        if (obj[key].length < 3) {
+            error = '用户或密码不可小于3位';
+        }
+    }
+    if (error) {
+        alert(error);
+        return;
+    }
+    ajax({  
+        type: "post",
+          url: url,
+          beforeSend: function () {},
+            //some js code 
+        data: obj,
+        success: function (data) {
+            var result = JSON.parse(data);
+            if (result.error) {
+                alert(result.error);
+            } else {
+                // alert('操作成功');
+                console.log(typeof result, '操作成功');
+                sessionStorage.setItem('user', data);
+            }
+        },
+        error: function () {
+            alert('系统异常，操作失败');
+        }
+    });
+}
+function getVaidlogin () {
+    ajax({  
+        type: "post",
+          url: "/vdidLogin",
+          beforeSend: function () {},
+            //some js code 
+        success: function (data) {
+            console.log(typeof data, data, 'vaid')
+            var result = JSON.parse(data);
+            if (result.error) {
+                var user = sessionStorage.getItem('user');
+                if (user) {
+                    user = JSON.parse(user);
+                    if (user.userName) {
+                        alert('登入失效，请重新登入');
+                        sessionStorage.setItem('user', '');
+                    }
+                }
+            } else {
+
+            }
+        },
+        error: function () {
+            alert('系统异常，操作失败');
+        }
+    });
 }
